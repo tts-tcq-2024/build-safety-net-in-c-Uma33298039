502f1931a491dc526897513ec3ad47c405869f02
Uma33298039 <umamageshwari098@gmail.com>
2024-07-04 22:44:13 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -5,28 +5,21 @@
 #include <ctype.h>
 #include <string.h>
 
-char getSoundexCode(char c) {
-    c = toupper(c);
-    switch (c) {
-        case 'B': case 'F': case 'P': case 'V': return '1';
-        case 'C': case 'G': case 'J': case 'K': case 'Q': case 'S': case 'X': case 'Z': return '2';
-        case 'D': case 'T': return '3';
-        case 'L': return '4';
-        case 'M': case 'N': return '5';
-        case 'R': return '6';
-        default: return '0'; // For A, E, I, O, U, H, W, Y
-    }
-}
-
 void generateSoundex(const char *name, char *soundex) {
     int len = strlen(name);
+    if (len == 0) {
+        strcpy(soundex, "0000");
+        return;
+    }
+
     soundex[0] = toupper(name[0]);
+    char previousCode = getSoundexCode(name[0]);
     int sIndex = 1;
 
     for (int i = 1; i < len && sIndex < 4; i++) {
         char code = getSoundexCode(name[i]);
-        if (code != '0' && code != soundex[sIndex - 1]) {
-            soundex[sIndex++] = code;
+        if (code != '0' && code != previousCode) {
+            soundex[sIndex++] = previousCode = code;
         }
     }
 
