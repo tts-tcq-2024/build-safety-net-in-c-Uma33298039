6b11d04a6846584b9ddb8c483484c074fe2f061e
Uma33298039 <umamageshwari098@gmail.com>
2024-07-08 09:53:46 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -1,18 +1,45 @@
-#include <gtest/gtest.h> 
-#include "Soundex.h" // Test fixture class for Soundex tests 
-class SoundexTest : public ::testing::Test 
-{ protected: char soundex[5]; 
- void assertSoundex(const char* name, const char* expected) 
-{ generateSoundex(name, soundex); 
-ASSERT_STREQ(soundex, expected); } }; 
-TEST_F(SoundexTest, EmptyName)
- { assertSoundex("", "0000"); } 
-TEST_F(SoundexTest, ShortName) 
-{ assertSoundex("Bob", "B000"); } 
-TEST_F(SoundexTest, LongName) 
-{ assertSoundex("Washington", "W252"); } 
-TEST_F(SoundexTest, RepeatedCharacters) 
-{ assertSoundex("Johnson", "J525"); } 
-TEST_F(SoundexTest, MixedCase) 
-{ assertSoundex("McDonald", "M235"); }
- int main(int argc, char **argv) { ::testing::InitGoogleTest(&argc, argv); return RUN_ALL_TESTS(); } 
+#include <gtest/gtest.h>
+#include "Soundex.h"
+ 
+TEST(SoundexTestsuite, ConstantsReplacedByDigits) {
+    char soundex[5];
+    generateSoundex("AX", soundex);
+    ASSERT_STREQ(soundex, "A200");
+}
+ 
+TEST(SoundexTestsuite, NonAlphabeticCharactersIgnored) {
+    char soundex[5];
+    generateSoundex("A1X", soundex);
+    ASSERT_STREQ(soundex, "A200");
+}
+
+TEST(SoundexTestsuite, HandlesEmptyString) {
+    char soundex[5];
+    generateSoundex("", soundex);
+    ASSERT_STREQ(soundex, "0000");
+}
+ 
+TEST(SoundexTestsuite,HandlesDifferentSoundexCodesForSameChar) {
+    char soundex[5];
+    generateSoundex("CGJKQSXZ", soundex);
+    ASSERT_STREQ(soundex, "C000");
+}
+ 
+TEST(SoundexTestsuite, HandlesVowelCharacters) {
+    char soundex[5];
+    generateSoundex("AEIOUHWY", soundex);
+    ASSERT_STREQ(soundex, "A000");
+}
+ 
+TEST(SoundexTestsuite, HandlesMultipleSameSoundexCodeCharacters) {
+    char soundex[5];
+    generateSoundex("TTTT", soundex);
+    ASSERT_STREQ(soundex, "T000");
+}
+ 
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+
+}
+ 
