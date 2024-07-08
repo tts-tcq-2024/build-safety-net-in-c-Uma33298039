57a8cd5c460161a3c9f185f7c742a456482bb07b
Uma33298039 <umamageshwari098@gmail.com>
2024-07-08 09:46:16 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -1,40 +1,23 @@
-#ifndef SOUNDEX_H
-#define SOUNDEX_H
-
-#include "Soundex.h"
-#include <ctype.h>
-#include <string.h>
-
-char getSoundexCode(char c) {
-    c = toupper(c);
-    switch (c) {
-        case 'B': case 'F': case 'P': case 'V': return '1';
-        case 'C': case 'G': case 'J': case 'K': case 'Q': case 'S': case 'X': case 'Z': return '2';
-        case 'D': case 'T': return '3';
-        case 'L': return '4';
-        case 'M': case 'N': return '5';
-        case 'R': return '6';
-        default: return '0'; // For A, E, I, O, U, H, W, Y
-    }
-}
-
-void generateSoundex(const char *name, char *soundex) {
-    int len = strlen(name);
-    soundex[0] = toupper(name[0]);
-    int sIndex = 1;
-
-    for (int i = 1; i < len && sIndex < 4; i++) {
-        char code = getSoundexCode(name[i]);
-        if (code != '0' && code != soundex[sIndex - 1]) {
-            soundex[sIndex++] = code;
-        }
-    }
-
-    while (sIndex < 4) {
-        soundex[sIndex++] = '0';
-    }
-
-    soundex[4] = '\0';
-}
-
-#endif // SOUNDEX_H
+#include "soundex.h"
+#include <ctype.h> 
+#include <string.h> // Helper function to get Soundex code for a character 
+static char getSoundexCode(char c) 
+{ static const char Map[26] = { 
+'0', '1', '2', '3', '0', '1', '2', '0', '0', '2', '2', '4', '5', '5', '0', '1', '2', '6', '2', '3', '0', '0', '0', '2', '0', '2' }; 
+return isalpha(c) ? Map[toupper(c) - 'A'] : '0';
+ } // Function to generate Soundex code for a given name 
+void generateSoundex(const char *name, char *soundex)
+{ 
+    if (name[0] == '\0') 
+    { 
+        strcpy(soundex, "0000"); return; 
+    } 
+soundex[0] = toupper(name[0]);
+int index = 1; // Start after the initial character 
+for (int i = 1; name[i] != '\0' && index < 4; i++) 
+{ char code = getSoundexCode(name[i]);
+ if (code != '0' && code != soundex[index - 1]) 
+{ soundex[index++] = code; } } 
+while (index < 4) { soundex[index++] = '0'; } 
+soundex[4] = '\0'; // Null terminate the string
+ } 
